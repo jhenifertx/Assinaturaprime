8cbb35a8a74a3958a858868a9f20aa04a9cff0f0
Jhenifer Teixeira <seu-email-do-github@exemplo.com>
2026-04-01 22:15:31 -0300

    style/ux: lower audio volume and replace with a cleaner success sound

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -125,8 +125,8 @@ export default function App() {
       setTimeout(() => {
         setIsGenerating(false);
         try {
-          const audio = new Audio('https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3');
-          audio.volume = 0.5;
+          const audio = new Audio('https://assets.mixkit.co/active_storage/sfx/2571/2571-preview.mp3');
+          audio.volume = 0.2;
           audio.play().catch(e => console.log('Audio error:', e));
         } catch (err) {
           // ignore
